f55669201e26d9eee4af516080c8bd52eace51c0
root <Administrator@PC-20171104RNCU>
2017-12-02 00:16:35 +0800

    1

---
+++ src/assdf/assdf.java
@@ -0,0 +1,28 @@
+package assdf;
+
+import java.io.IOException;
+import java.net.ResponseCache;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class assdf extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public assdf() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("123");
+
+	}
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/nhao.java
@@ -0,0 +1,11 @@
+
+public class nhao {
+
+	public static void main(String[] args) {
+		String str1 = "你好";
+System.out.println(str1);
+		String str2 = new String("你好");
+		System.out.println(str2);
+	}
+
+}
